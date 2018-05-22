4770f8140b7237de20d0154d35a7590066308ec1
slaventiysvat <slaventiysvat@gmail.com>
2018-05-22 11:49:00 +0300

    Здесь хранятся файл проекта мел кепстральных коеффициентов и оценки интервалов между фонемами

---
+++ SetParam.m
@@ -0,0 +1,12 @@
+NFilt=20;%
+Fs=44100;%
+Fl=20;%low F
+Fh=44100;%high F
+samplesize=2048;
+choseFilter=[1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17];
+diff= NFilt - length(choseFilter);
+if diff ==0
+    conF=1;
+else
+conF=zeros(1,diff,'double');
+end
