e4bee5fda3d74d45e247f171fe55a839af5d8e62
slaventiysvat <slaventiysvat@gmail.com>
2018-05-26 15:42:59 +0300

    незначительные изменения в синтаксис без влияния на принцип работы

--- SetParam.m
+++ SetParam.m
@@ -6,7 +6,8 @@ samplesize=2048;
 choseFilter=[1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17];
 diff= NFilt - length(choseFilter);
 if diff ==0
-    conF=1;
+    conF=1;%определяет сколько елементов в 20 ступенастом фильтре не хватает
+    
 else
 conF=zeros(1,diff,'double');
 end
